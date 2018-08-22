ad9fb80cd70e9352c5e932c46c7259db3aebe4a9
Denys Vitali <denys@denv.it>
2018-08-22 15:33:46 +0200

    Automatically generate Error Codes (#4)

---
+++ generate.py
@@ -0,0 +1,20 @@
+import requests
+
+r = requests.get('http://webconcepts.info/concepts/http-status-code.json')
+json = r.json()
+
+for i in json["values"]:
+    template = "templates/" + i["value"][0:1] + "xx.html"
+    with open(template) as f:
+        content = f.read()
+        new_content = content
+        new_content = new_content.replace("$ERROR_CODE", i["value"])
+        new_content = new_content.replace("$ERROR_NAME", i["description"])
+        new_content = new_content.replace("$ERROR_DESC", i["details"][0]["description"])
+        with open(i["value"] + ".html", "w") as output_file:
+            output_file.write(new_content)
+
+with open("snippets/error_pages_content.conf", "w") as epc:
+    for i in json["values"]:
+        v = int(i["value"])
+        print("error_page %d /%d.html;" % (v,v), file=epc)
